cb651f686b483898fe57f7e8f2fb62c80109a860
tri-suli <trisulipras@gmail.com>
2024-03-11 22:34:19 +0700

    Add login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3 className={'text-center'}>{'Login Form'}</h3>
+        <form action="" noValidate>
+          <div className="input-container">
+            <label htmlFor="email">{'Email'}</label>
+            <input type="email" />
+          </div>
+          <div className="input-container">
+            <label htmlFor="password">{'Password'}</label>
+            <input type="password" />
+          </div>
+          <div className="action">
+            <button className="submit">{'Login'}</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
