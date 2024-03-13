305a210ec4ceb016d72c99136a85aa46efb29ae6
tri-suli <trisulipras@gmail.com>
2024-03-13 15:24:55 +0700

    Refactor App.tsx and improve CSS
    Refactored the App component to include the new LoginForm component, replacing redundant form code, and added more specific tests to App.test.tsx. The CSS file was also edited to include styling for better form interaction and some additional UI improvements.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+    const linkElement = screen.getByText('Login Form');
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('renders LoginForm component', () => {
+    render(<App />);
+    const loginFormElement = screen.getByTestId("loginForm");
+    expect(loginFormElement).toBeInTheDocument();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import LoginForm from './components/form/LoginForm';
+
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h3 className={'text-center'}>{'Login Form'}</h3>
-        <form action="" noValidate>
-          <div className="input-container">
-            <label htmlFor="email">{'Email'}</label>
-            <input type="email" />
-          </div>
-          <div className="input-container">
-            <label htmlFor="password">{'Password'}</label>
-            <input type="password" />
-          </div>
-          <div className="action">
-            <button className="submit">{'Login'}</button>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </div>
   );
